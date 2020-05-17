c3f45717736c5d80e96bdcaa4469130cd60ba71d
Cameron DaCamara <cadacama@microsoft.com>
2020-05-17 00:37:45 -0700

    fixup

--- Videos/destroy.cpp
+++ Videos/destroy.cpp
@@ -35,6 +35,7 @@
 //
 // QuadTree:
 // * https://gamedevelopment.tutsplus.com/tutorials/quick-tip-use-quadtrees-to-detect-likely-collisions-in-2d-space--gamedev-374
+// * https://stackoverflow.com/questions/41946007/efficient-and-well-explained-implementation-of-a-quadtree-for-2d-collision-det
 //
 // % Chance:
 // * https://gamedev.stackexchange.com/questions/22891/designing-drops-system-how-and-where-chance-of-drops-are-defined
@@ -1716,6 +1717,165 @@ private:
     const Level level;
 };
 
+class QuadTreeNew
+{
+    static constexpr int max_depth = 5;
+    static constexpr int split_factor = 20;
+public:
+    QuadTreeNew(const olc::vi2d& upper_left, Width width, Height height, Level level):
+        rect{ upper_left, width, height }, level{ level } { }
+
+    void clear()
+    {
+        objects.clear();
+        trees = { }; // clear the existing trees.
+    }
+
+    void insert(PhysicsPixel* pixel)
+    {
+        internal_insert(box_for(*pixel), pixel);
+    }
+
+    template <std::invocable<PhysicsPixel*> F>
+    void for_each_in(const BoundingBox& box, F&& invocable) const
+    {
+        int i = index(box);
+        if (i != -1 && trees[i])
+        {
+            trees[i]->for_each_in(box, std::forward<F>(invocable));
+        }
+        for (PhysicsPixel* pixel : objects)
+        {
+            std::forward<F>(invocable)(pixel);
+        }
+    }
+
+    static BoundingBox box_for(const PhysicsPixel& pixel)
+    {
+        int r = rep(pixel.radius());
+        return { pixel.position(), Width(r), Height(r) };
+    }
+
+    std::vector<BoundingBox> all_boxes() const
+    {
+        std::vector<BoundingBox> boxes;
+        internal_all_boxes(&boxes);
+        return boxes;
+    }
+private:
+    void internal_all_boxes(std::vector<BoundingBox>* boxes) const
+    {
+        boxes->push_back(rect);
+
+        for (const auto& tree : trees)
+        {
+            if (tree)
+            {
+                tree->internal_all_boxes(boxes);
+            }
+        }
+    }
+
+    void internal_insert(const BoundingBox& box, PhysicsPixel* pixel)
+    {
+        if (trees[0])
+        {
+            int i = index(box);
+            if (i != -1)
+            {
+                trees[i]->internal_insert(box, pixel);
+                return;
+            }
+        }
+
+        objects.push_back(pixel);
+
+        if (objects.size() > split_factor && rep(level) < max_depth)
+        {
+            if (!trees[0])
+            {
+                split();
+            }
+
+            int i = 0;
+            while (i < static_cast<int>(objects.size()))
+            {
+                BoundingBox object_box = box_for(*objects[i]);
+                int idx = index(object_box);
+                if (idx != -1)
+                {
+                    trees[idx]->internal_insert(object_box, objects[i]);
+                    objects.erase(begin(objects) + i);
+                }
+                else
+                {
+                    ++i;
+                }
+            }
+        }
+    }
+
+    void split()
+    {
+        int sub_width = rep(rect.width()) / 2;
+        int sub_height = rep(rect.height()) / 2;
+        int x = rect.left();
+        int y = rect.top();
+        int next_level = rep(level) + 1;
+
+        trees[0] = std::make_unique<QuadTreeNew>(olc::vi2d{ x + sub_width, y },              Width(sub_width), Height(sub_height), Level(next_level));
+        trees[1] = std::make_unique<QuadTreeNew>(olc::vi2d{ x,             y },              Width(sub_width), Height(sub_height), Level(next_level));
+        trees[2] = std::make_unique<QuadTreeNew>(olc::vi2d{ x,             y + sub_height }, Width(sub_width), Height(sub_height), Level(next_level));
+        trees[3] = std::make_unique<QuadTreeNew>(olc::vi2d{ x + sub_width, y + sub_height }, Width(sub_width), Height(sub_height), Level(next_level));
+    }
+
+    int index(const BoundingBox& box) const
+    {
+        int index = -1;
+        float vert_mid = rect.left() + static_cast<float>(rep(rect.width())) / 2.f;
+        float horiz_mid = rect.top() + static_cast<float>(rep(rect.height())) / 2.f;
+
+        auto is_top = [&]
+        {
+            return box.top() < horiz_mid && box.bottom() < horiz_mid;
+        };
+        auto is_bottom = [&]
+        {
+            return box.top() > horiz_mid;
+        };
+
+        if (box.left() <= vert_mid && box.right() <= vert_mid)
+        {
+            if (is_top())
+            {
+                index = 1;
+            }
+            else if (is_bottom())
+            {
+                index = 2;
+            }
+        }
+        else if (box.left() >= vert_mid)
+        {
+            if (is_top())
+            {
+                index = 0;
+            }
+            else if (is_bottom())
+            {
+                index = 3;
+            }
+        }
+
+        return index;
+    }
+
+    std::array<std::unique_ptr<QuadTreeNew>, max_depth> trees;
+    std::vector<PhysicsPixel*> objects;
+    BoundingBox rect;
+    const Level level;
+};
+
 class PhysicsEngine
 {
     static constexpr int d_time = 16;
@@ -1764,7 +1924,7 @@ public:
 
                 pixel.execute_step(world);
 
-#if 1
+#if 0
                 if (all_collisions)
                 {
                     quad_tree->insert(&pixel);
@@ -1777,7 +1937,7 @@ public:
             // If we are doing extra interactions, do them.
             if (all_collisions)
             {
-#if 0
+#if 1
                 stopwatch.start();
                 for (auto& pixel : pixels_objects)
                 {
